73564ea9a98e7be4696d07262e0a974cb2d3cd8c
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-08-31 11:27:17 +0200

    Refactor test file

--- junit/src/Assertions.java
+++ junit/src/Assertions.java
@@ -8,15 +8,7 @@ public class Assertions {
   void assertNull(Object actual) { }
   void assertNotNull(Object actual) { }
 
-  void assertEquals(short expected, short actual) { }
-  void assertEquals(byte expected, byte actual) { }
-  void assertEquals(int expected, int actual) { }
-  void assertEquals(long expected, long actual) { }
-  void assertEquals(char expected, char actual) { }
-  void assertEquals(float expected, float actual) { }
-  void assertEquals(double expected, double actual) { }
   void assertEquals(Object expected, Object actual) { }
-
   void assertNotEquals(Object unexpected, Object actual) { }
 
   void assertArrayEquals(Object[] expected, Object[] actual) { }
@@ -27,6 +19,6 @@ public class Assertions {
   void assertAll(Collection<Executable> executables) { }
   void assertTimeout(Duration timeout, Executable executable) { }
 
-  <T extends Throwable> T assertThrows(Class<T> expectedType, Executable executable) { }
+  <T extends Throwable> T assertThrows(Class<T> expectedType, Executable exec) { }
 }
 // end::adocSnippet[]
